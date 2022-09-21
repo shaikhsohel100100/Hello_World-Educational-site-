db55a6678480461688348203746b142b20b71512
Shaikh_Sohel <93519202+shaikhsohel100100@users.noreply.github.com>
2022-09-21 21:16:48 +0530

    Add files via upload

---
+++ Tutorial.js
@@ -0,0 +1,21 @@
+// console.log("heiii")
+/*
+
+let tut1Id = document.getElementById("tut-1");
+let tut2Id = document.getElementById("tut-2");
+
+function change1(){
+    let iframeWindow = tut1Id.contentWindow;
+    let iframeDocument = tut1Id.contentDocument;
+
+    // tut1Id.replaceWith(tut2Id);
+    // console.log(tut1Id.innerHTML);
+    location.reload();
+    // console.log(iframeWindow.document);
+}
+function change2(){
+    location.reload();
+    tut1Id.replaceWith(tut2Id);
+}
+
+*/
